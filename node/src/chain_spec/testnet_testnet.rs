f211398ab0d5cf43a033536b9758af7e3960542a
Daniel Olano <daniel@olanod.com>
2020-10-20 15:38:41 +0200

    Give testnet nodes vanity addresses

--- node/src/chain_spec/testnet_testnet.rs
+++ node/src/chain_spec/testnet_testnet.rs
@@ -7,14 +7,14 @@ use sp_core::sr25519;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use vln_runtime::WASM_BINARY;
 
-const ODIN_AURA_SS58: &str = "5HSxWDrQCtoM49VWt1HYaywGrSxwEJyn6AsbDVAoRMEevyU7";
-const ODIN_GRANDPA_SS58: &str = "5CE3jbWckmKMaQnv8ECEscKJJNwCPJXrm8jPAjNf765qywvK";
+const ODIN_AURA_SS58: &str = "5DodinBNh59Ew9bvRfoDmj1j1QXzPhvHqk5w6jpdAfq7DHZd";
+const ODIN_GRANDPA_SS58: &str = "5CodinauszfC7FeAnugMk1eXiBCsrJmg4pE6RQN9GzQ7nEpb";
 
-const THOR_AURA_SS58: &str = "5EaAVzfBcGWfzb4ZWMRcoxwweJk33yhzcPWmiurxmWVAPVhK";
-const THOR_GRANDPA_SS58: &str = "5FaC77dXnZkx4nm12omhyJc5xcUvHCJ8Z4MxJ9Gpa14WTqhu";
+const THOR_AURA_SS58: &str = "5Cthor4p3N8XMhYRjsKta7nXv2FsBc8gmX7p9pDAoejEeAhR";
+const THOR_GRANDPA_SS58: &str = "5GthorY858eiYyC48XPtL6fWDpmvv4tM6qaBRHuPc4sjER5V";
 
-const LOKI_AURA_SS58: &str = "5EZeDVGEnG2SXKdHQPsimbJzB5tx18d1dMAocRvaPM2K2XCR";
-const LOKI_GRANDPA_SS58: &str = "5ELYs2C7ePxsSNFZBHo5v5Rgq18fQzwepMzbdn43j5i724va";
+const LOKI_AURA_SS58: &str = "5CLokibKMfc9NfPWaEzEqkLAL2JkZLA3hZNGXxvSgCJkSgTQ";
+const LOKI_GRANDPA_SS58: &str = "5ELokiYDQEjTGk1hDSKuVusyHyuYYa1e8JGEc7gfiY9f3eey";
 
 pub fn chain_spec() -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or_else(|| "WASM binary not available".to_string())?;
