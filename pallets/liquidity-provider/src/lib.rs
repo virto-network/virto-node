b05faa5bb72a3338d4cd6e460b33f0ee5ece9be5
Caio <c410.f3r@gmail.com>
2020-10-20 02:33:50 -0300

    Lock attested assets (#12)

--- pallets/liquidity-provider/src/lib.rs
+++ pallets/liquidity-provider/src/lib.rs
@@ -2,12 +2,11 @@
 
 use frame_support::{decl_error, decl_event, decl_module, dispatch};
 use frame_system::ensure_signed;
-use orml_traits::MultiCurrency;
+use orml_traits::{MultiCurrency, MultiReservableCurrency};
 use valiu_node_commons::Asset;
 
 #[cfg(test)]
 mod mock;
-
 #[cfg(test)]
 mod tests;
 
@@ -15,9 +14,8 @@ type BalanceOf<T> =
     <<T as Trait>::Currency as MultiCurrency<<T as frame_system::Trait>::AccountId>>::Balance;
 
 pub trait Trait: pallet_membership::Trait {
-    /// Because this pallet emits events, it depends on the runtime's definition of an event.
+    type Currency: MultiReservableCurrency<Self::AccountId, CurrencyId = Asset>;
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
-    type Currency: MultiCurrency<Self::AccountId, CurrencyId = Asset>;
 }
 
 decl_event!(
@@ -25,16 +23,14 @@ decl_event!(
     where
         AccountId = <T as frame_system::Trait>::AccountId,
     {
-        /// Event documentation should end with an array that provides descriptive names for event
-        /// parameters. [something, who]
         Attestation(AccountId, Asset),
     }
 );
 
 decl_error! {
     pub enum Error for Module<T: Trait> {
+        MustNotBeUsdv,
         NotAProvider,
-        MustNotBeUsdv
     }
 }
 
@@ -56,6 +52,7 @@ decl_module! {
             members.binary_search(&provider).map_err(|_| Error::<T>::NotAProvider)?;
 
             T::Currency::deposit(asset_id, &provider, balance)?;
+            T::Currency::reserve(asset_id, &provider, balance)?;
             Self::deposit_event(RawEvent::Attestation(provider, asset_id));
             Ok(())
         }
