505b04c914362201115a0e6d5f0abbfe096137ca
Caio <c410.f3r@gmail.com>
2020-10-28 07:53:08 -0300

    Transfer collaterals (#16)

--- pallets/liquidity-provider/src/tests.rs
+++ pallets/liquidity-provider/src/tests.rs
@@ -2,9 +2,11 @@ use crate::mock::*;
 use frame_support::{assert_noop, assert_ok};
 use orml_traits::{MultiCurrency, MultiReservableCurrency};
 use sp_runtime::traits::BadOrigin;
-use valiu_node_commons::{Asset, Collateral};
+use valiu_node_commons::{Asset, Collateral, DistributionStrategy};
 
 const ROOT: u64 = 1;
+const USD_ASSET: Asset = Asset::Collateral(USD_COLLATERAL);
+const USD_COLLATERAL: Collateral = Collateral::Usd;
 const USDC_ASSET: Asset = Asset::Collateral(USDC_COLLATERAL);
 const USDC_COLLATERAL: Collateral = Collateral::Usdc;
 const USDV_ASSET: Asset = Asset::Usdv;
@@ -60,5 +62,37 @@ fn only_providers_can_attest() {
             USDC_COLLATERAL,
             123
         ));
+    });
+}
+
+#[test]
+fn usdv_transfer_also_transfers_collaterals() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(MintMembers::add_member(Origin::signed(ROOT), 2));
+        assert_ok!(ProviderMembers::add_member(Origin::signed(ROOT), 2));
+
+        assert_ok!(ProviderMembers::add_member(Origin::signed(ROOT), 3));
+
+        assert_ok!(TestProvider::attest(Origin::signed(2), USD_COLLATERAL, 60));
+        assert_ok!(TestProvider::attest(Origin::signed(2), USDC_COLLATERAL, 40));
+
+        assert_ok!(TestProvider::transfer(
+            Origin::signed(2),
+            3,
+            30,
+            DistributionStrategy::Evenly
+        ));
+
+        assert_eq!(Tokens::free_balance(USDV_ASSET, &2), 70);
+        assert_eq!(Tokens::free_balance(USDV_ASSET, &3), 30);
+
+        assert_eq!(Tokens::reserved_balance(USD_ASSET, &2), 42);
+        assert_eq!(Tokens::reserved_balance(USDC_ASSET, &2), 28);
+        assert_eq!(Tokens::reserved_balance(USD_ASSET, &3), 18);
+        assert_eq!(Tokens::reserved_balance(USDC_ASSET, &3), 12);
+
+        assert_eq!(Tokens::total_issuance(USD_ASSET), 60);
+        assert_eq!(Tokens::total_issuance(USDC_ASSET), 40);
+        assert_eq!(Tokens::total_issuance(USDV_ASSET), 100);
     });
 }
