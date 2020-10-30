1e0d66a482d2f82c35591d1db917135009b2c27e
Caio <c410.f3r@gmail.com>
2020-10-30 18:16:39 -0300

    Provide and update a list of offers (#18)

--- pallets/liquidity-provider/src/lib.rs
+++ pallets/liquidity-provider/src/lib.rs
@@ -1,10 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_error, decl_event, decl_module, dispatch};
+extern crate alloc;
+
+use alloc::vec::Vec;
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
 use frame_system::ensure_signed;
 use orml_traits::{MultiCurrency, MultiReservableCurrency};
 use sp_arithmetic::traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub};
-use valiu_node_commons::{Asset, DistributionStrategy};
+use valiu_node_commons::{AccountRate, Asset, DistributionStrategy, OfferRate};
 
 #[cfg(test)]
 mod mock;
@@ -12,8 +15,10 @@ mod mock;
 mod tests;
 mod transfer_handlers;
 
+type AccountRateTy<T> = AccountRate<<T as frame_system::Trait>::AccountId, Balance<T>>;
 type Balance<T> =
     <<T as Trait>::Collateral as MultiCurrency<<T as frame_system::Trait>::AccountId>>::Balance;
+type OfferRateTy<T> = OfferRate<Balance<T>>;
 type ProviderMembers = pallet_membership::DefaultInstance;
 
 pub trait Trait: pallet_membership::Trait<ProviderMembers>
@@ -56,13 +61,15 @@ decl_module! {
         pub fn attest(
             origin,
             asset: Asset,
-            balance: Balance<T>
+            balance: Balance<T>,
+            offer_rates: Vec<OfferRateTy<T>>
         ) -> dispatch::DispatchResult
         {
             match asset {
                 Asset::Usdv => return Err(crate::Error::<T>::MustNotBeUsdv.into()),
                 Asset::Collateral(collateral) => {
                     let who = ensure_signed(origin)?;
+                    update_account_rates::<T>(&who, asset, offer_rates);
                     do_attest::<T>(who.clone(), Asset::Usdv, balance)?;
                     T::Collateral::deposit(collateral.into(), &who, balance)?;
                     T::Collateral::reserve(collateral.into(), &who, balance)?;
@@ -86,6 +93,35 @@ decl_module! {
             }
             Ok(())
         }
+
+        #[weight = 0]
+        pub fn update_offer_rates(
+            origin,
+            asset: Asset,
+            offer_rates: Vec<OfferRateTy<T>>
+        ) -> dispatch::DispatchResult
+        {
+            let who = ensure_signed(origin)?;
+            update_account_rates::<T>(&who, asset, offer_rates);
+            Ok(())
+        }
+    }
+}
+
+impl<T> Module<T>
+where
+    T: Trait,
+{
+    pub fn account_rates(from_asset: &Asset, to_asset: &Asset) -> Vec<AccountRateTy<T>> {
+        <Offers<T>>::get(from_asset, to_asset)
+    }
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as Tokens {
+        pub Offers get(fn accounts):
+            double_map hasher(twox_64_concat) Asset,
+            hasher(twox_64_concat) Asset => Vec<AccountRateTy<T>>
     }
 }
 
@@ -105,4 +141,26 @@ where
     T::Asset::deposit(asset, &from, balance)?;
     Module::<T>::deposit_event(RawEvent::Attestation(from, asset));
     Ok(())
+}
+
+#[inline]
+fn update_account_rates<T>(
+    from: &<T as frame_system::Trait>::AccountId,
+    asset: Asset,
+    offer_rates: Vec<OfferRateTy<T>>,
+) where
+    T: Trait,
+{
+    for offer_rate in offer_rates {
+        <Offers<T>>::mutate(asset, offer_rate.asset(), |account_rates| {
+            let idx = account_rates
+                .binary_search_by(|el| el.account().cmp(from))
+                .unwrap_or_else(|idx| idx);
+            if let Some(rslt) = account_rates.get_mut(idx) {
+                *rslt.rate_mut() = *offer_rate.rate();
+            } else {
+                account_rates.insert(idx, AccountRate::new(from.clone(), *offer_rate.rate()))
+            }
+        })
+    }
 }
--- valiu-node-commons/src/lib.rs
+++ valiu-node-commons/src/lib.rs
@@ -1,9 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod account_rate;
 mod asset;
 mod collateral;
 mod distribution_strategy;
+mod offer_rate;
 
+pub use account_rate::*;
 pub use asset::*;
 pub use collateral::*;
 pub use distribution_strategy::*;
+pub use offer_rate::*;
