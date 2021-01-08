7e2d50ee2f5d0b9699775adabb07cee2e23ca85a
Stanly Johnson <stanlyjohnson@outlook.com>
2021-01-08 21:56:47 +0530

    reduce diff size

--- valiu-node-commons/src/asset.rs
+++ valiu-node-commons/src/asset.rs
@@ -34,6 +34,13 @@ impl Asset {
     }
 }
 
+impl Default for Asset {
+    #[inline]
+    fn default() -> Self {
+        Self::Btc
+    }
+}
+
 impl fmt::Display for Asset {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -45,12 +52,5 @@ impl From<Collateral> for Asset {
     #[inline]
     fn from(from: Collateral) -> Self {
         Asset::Collateral(from)
-    }
-}
-
-impl Default for Asset {
-    #[inline]
-    fn default() -> Self {
-        Self::Btc
     }
 }
