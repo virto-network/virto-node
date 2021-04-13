d82db91bc245479ea57a9bc75ea03b13f21140a7
Stanly Johnson <stanlyjohnson@outlook.com>
2021-04-13 12:03:24 +0530

    move currencyconvert to new file

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -46,13 +46,15 @@ mod standalone_use {
     pub use sp_runtime::traits::NumberFor;
 }
 
+#[cfg(not(feature = "standalone"))]
+mod currency_id_convert;
+
 // XCM imports
 #[cfg(not(feature = "standalone"))]
 use parachain_use::*;
 #[cfg(not(feature = "standalone"))]
 mod parachain_use {
-    pub use codec::{Decode, Encode};
-    pub use cumulus_primitives_core::ParaId;
+    pub use crate::currency_id_convert::CurrencyIdConvert;
     pub use frame_system::EnsureRoot;
     pub use orml_xcm_support::{
         IsNativeConcrete, MultiCurrencyAdapter, MultiNativeAsset, XcmHandler as XcmHandlerT,
@@ -65,9 +67,8 @@ mod parachain_use {
     pub use sp_std::collections::btree_set::BTreeSet;
     pub use vln_primitives::{ForeignCurrencyId, TokenSymbol};
     pub use xcm::v0::{
-        Junction::{GeneralKey, Parachain, Parent},
-        MultiAsset,
-        MultiLocation::{self, X1, X2, X3},
+        Junction::{Parachain, Parent},
+        MultiLocation::{self, X1, X2},
         NetworkId, Xcm,
     };
     pub use xcm_builder::{
@@ -540,64 +541,6 @@ mod parachain_impl {
     impl XcmHandlerT<AccountId> for HandleXcm {
         fn execute_xcm(origin: AccountId, xcm: Xcm) -> DispatchResult {
             XcmHandler::execute_xcm(origin, xcm)
-        }
-    }
-
-    fn native_currency_location(id: ForeignCurrencyId) -> MultiLocation {
-        X3(
-            Parent,
-            Parachain {
-                id: ParachainInfo::get().into(),
-            },
-            GeneralKey(id.encode()),
-        )
-    }
-
-    pub struct CurrencyIdConvert;
-    impl Convert<ForeignCurrencyId, Option<MultiLocation>> for CurrencyIdConvert {
-        fn convert(id: ForeignCurrencyId) -> Option<MultiLocation> {
-            use ForeignCurrencyId::Token;
-            use TokenSymbol::*;
-            match id {
-                Token(DOT) => Some(X1(Parent)),
-                Token(ACA) | Token(AUSD) => Some(native_currency_location(id)),
-                _ => None,
-            }
-        }
-    }
-
-    impl Convert<MultiLocation, Option<ForeignCurrencyId>> for CurrencyIdConvert {
-        fn convert(location: MultiLocation) -> Option<ForeignCurrencyId> {
-            use ForeignCurrencyId::Token;
-            use TokenSymbol::*;
-            match location {
-                X1(Parent) => Some(Token(DOT)),
-                X3(Parent, Parachain { id }, GeneralKey(key))
-                    if ParaId::from(id) == ParachainInfo::get() =>
-                {
-                    // decode the general key
-                    if let Ok(currency_id) = ForeignCurrencyId::decode(&mut &key[..]) {
-                        // check if `currency_id` is cross-chain asset
-                        match currency_id {
-                            Token(ACA) | Token(AUSD) => Some(currency_id),
-                            _ => None,
-                        }
-                    } else {
-                        None
-                    }
-                }
-                _ => None,
-            }
-        }
-    }
-
-    impl Convert<MultiAsset, Option<ForeignCurrencyId>> for CurrencyIdConvert {
-        fn convert(asset: MultiAsset) -> Option<ForeignCurrencyId> {
-            if let MultiAsset::ConcreteFungible { id, amount: _ } = asset {
-                Self::convert(id)
-            } else {
-                None
-            }
         }
     }
 
