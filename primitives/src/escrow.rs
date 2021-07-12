7561651609b65320c6993950081420b4e692e888
Daniel Olano <daniel@olanod.com>
2021-07-12 19:15:20 +0200

    Handle release of held assets failure

--- primitives/src/escrow.rs
+++ primitives/src/escrow.rs
@@ -1,7 +1,7 @@
 #![allow(unused_qualifications)]
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use sp_runtime::DispatchError;
+use sp_runtime::DispatchResult;
 
 #[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, TypeInfo)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -17,6 +17,8 @@ pub enum EscrowState {
     Created,
     Released,
     Cancelled,
+    /// A judge needs to review and release manually
+    NeedsReview,
 }
 
 // trait that defines how to create/release escrows for users
@@ -24,19 +26,14 @@ pub trait EscrowHandler<Account, Asset, Amount> {
     /// Attempt to reserve an amount of the given asset from the caller
     /// If not possible then return Error. Possible reasons for failure include:
     /// - User does not have enough balance.
-    fn create_escrow(
-        from: Account,
-        to: Account,
-        asset: Asset,
-        amount: Amount,
-    ) -> Result<(), DispatchError>;
+    fn create_escrow(from: Account, to: Account, asset: Asset, amount: Amount) -> DispatchResult;
 
     /// Attempt to transfer an amount of the given asset from the given escrow_id
     /// If not possible then return Error. Possible reasons for failure include:
     /// - The escrow does not exist
     /// - The unreserve operation fails
     /// - The transfer operation fails
-    fn release_escrow(from: Account, to: Account) -> Result<(), DispatchError>;
+    fn release_escrow(from: Account, to: Account) -> DispatchResult;
 
     /// Attempt to cancel an escrow in Created state. This will set the escrow
     /// state to cancel and release the reserved amount back to the creator.
@@ -44,7 +41,7 @@ pub trait EscrowHandler<Account, Asset, Amount> {
     /// - The escrow does not exist
     /// - The escrow is not in Created state
     /// - The unreserve operation fails
-    fn cancel_escrow(from: Account, to: Account) -> Result<(), DispatchError>;
+    fn cancel_escrow(from: Account, to: Account) -> DispatchResult;
 
     /// Attempt to fetch the details of an escrow from the given escrow_id
     /// Possible reasons for failure include:
