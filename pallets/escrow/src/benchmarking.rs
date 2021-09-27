f7764aefa65c8c9833a05125ef8d1908ebd8c69a
Stanly Johnson <stanlyjohnson@outlook.com>
2021-09-27 12:11:51 +0530

    more fixes

--- pallets/escrow/src/benchmarking.rs
+++ pallets/escrow/src/benchmarking.rs
@@ -1,12 +1,66 @@
 #[cfg(feature = "runtime-benchmarks")]
+use super::*;
+use orml_traits::MultiCurrency;
+use frame_benchmarking::{benchmarks, account, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::RawOrigin;
 
-  use crate::{*, Pallet as Escrow};
-  use frame_benchmarking::{benchmarks, account, impl_benchmark_test_suite};
-  use frame_system::RawOrigin;
+const SEED: u32 = 0;
 
-  benchmarks!{
-    // Individual benchmarks are placed here
+  benchmarks! {
+    // the longest path in create extrinsic is succesful create
+    create {
+      let caller: T::AccountId = whitelisted_caller();
+      let to: T::AccountId = account("to", 1, SEED);
+      T::Tokens::make_free_balance_be(&caller, 100.into());
+      const CURRENCY_ID: u32 = 2;
+    }: _(RawOrigin::Signed(caller), to, CURRENCY_ID, 1.into())
+    verify {
+			// the escrow amount should be reserved
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &caller), 80);
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &to), 0);
+		}
+
+    // the longest path in release extrinsic is successful release
+    release {
+      let caller: T::AccountId = whitelisted_caller();
+      let to: T::AccountId = account("to", 1, SEED);
+      T::Tokens::make_free_balance_be(&caller, 100.into());
+      // create escrow
+    }: _(RawOrigin::Signed(caller), to)
+    verify {
+			// the escrow amount should be reserved
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &caller), 80);
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &to), 20);
+		}
+
+    // the longest path in cancel extrinsic is a succesful cancel
+    cancel {
+      let caller: T::AccountId = whitelisted_caller();
+      let to: T::AccountId = account("to", 1, SEED);
+      T::Tokens::make_free_balance_be(&caller, 100.into());
+      const CURRENCY_ID: u32 = 2;
+      // create escrow
+    }: _(RawOrigin::Signed(caller), to)
+    verify {
+			// the escrow amount should be reserved
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &caller), 100);
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &to), 0);
+		}
+
+    // the longest path in resolve extrinsic is to release or cancel
+    resolve {
+      let caller: T::AccountId = whitelisted_caller();
+      let to: T::AccountId = account("to", 1, SEED);
+      T::Tokens::make_free_balance_be(&caller, 100.into());
+      const CURRENCY_ID: u32 = 2;
+      // create escrow
+    }: _(RawOrigin::Signed(caller), to, CURRENCY_ID, 1.into())
+    verify {
+			// the escrow amount should be reserved
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &caller), 80);
+      assert_eq!(crate::mock::Tokens::free_balance(CURRENCY_ID, &to), 0);
+		}
   }
 
 
-  impl_benchmark_test_suite!(Escrow, crate::tests::new_test_ext(), crate::tests::Test);
+  impl_benchmark_test_suite!(Escrow, crate::mock::new_test_ext(), crate::mock::Test);
