3d5230ba8faa2f631ef99942d1ce95704e822d9c
Stanly Johnson <stanlyjohnson@outlook.com>
2021-12-12 13:58:09 +0000

    fix benchmarks

--- pallets/payment/src/benchmarking.rs
+++ pallets/payment/src/benchmarking.rs
@@ -8,18 +8,6 @@ use virto_primitives::PaymentState;
 
 const SEED: u32 = 0;
 
-fn setup_caller<T: Config>() -> T::AccountId {
-	let caller = whitelisted_caller();
-	let value: BalanceOf<T> = 10u32.into();
-	let asset: AssetIdOf<T> = "test".into(); // TODO : figure out way to convert to type of CurrencyId of orml-tokens
-	let _ = T::Asset::deposit(asset, &caller, value);
-	caller
-}
-
-fn get_asset_and_balance<T: Config>(asset_id: u32, balance: u32) -> (AssetIdOf<T>, BalanceOf<T>) {
-	(asset_id.into(), balance.into())
-}
-
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
 	let events = frame_system::Pallet::<T>::events();
 	let system_event: <T as frame_system::Config>::Event = generic_event.into();
@@ -29,45 +17,50 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
 }
 
 benchmarks! {
+	where_clause { where T::Asset: MultiCurrency<
+		<T as frame_system::Config>::AccountId,
+		CurrencyId = u32, Balance = u32
+	>
+}
 	// create a new payment succesfully
 	create {
-		let caller: T::AccountId = setup_caller::<T>();
+		let caller = whitelisted_caller();
+		let _ = T::Asset::deposit(1u32, &caller, 100u32);
 		let recipent = account("recipient", 0, SEED);
-		let (asset, amount) = get_asset_and_balance::<T>(1,5);
-	}: _(RawOrigin::Signed(caller), recipent, asset, amount)
+	}: _(RawOrigin::Signed(caller.clone()), recipent, 1u32, 80u32)
 	verify {
-		assert_last_event::<T>(Event::<T>::PaymentCreated(caller, asset, amount).into());
+		assert_last_event::<T>(Event::<T>::PaymentCreated(caller, 1u32, 2u32).into());
 	}
 
 	// release an existing payment succesfully
 	release {
-		let caller: T::AccountId = setup_caller::<T>();
-		let recipent = account("recipient", 0, SEED);
-		let (asset, amount) = get_asset_and_balance::<T>(1,5);
-		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent, asset, amount);
-	}: _(RawOrigin::Signed(caller), recipent)
+		let caller = whitelisted_caller();
+		let _ = T::Asset::deposit(1u32, &caller, 100u32);
+		let recipent : T::AccountId = account("recipient", 0, SEED);
+		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent.clone(), 1u32, 80u32)?;
+	}: _(RawOrigin::Signed(caller.clone()), recipent.clone())
 	verify {
 		assert_last_event::<T>(Event::<T>::PaymentReleased(caller, recipent).into());
 	}
 
-	// cancel an existing payment succesfully
+	// // cancel an existing payment succesfully
 	cancel {
-		let caller: T::AccountId = setup_caller::<T>();
-		let recipent = account("recipient", 0, SEED);
-		let (asset, amount) = get_asset_and_balance::<T>(1,5);
-		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent, asset, amount);
-	}: _(RawOrigin::Signed(caller), recipent)
+		let caller = whitelisted_caller();
+		let _ = T::Asset::deposit(1u32, &caller, 100u32);
+		let recipent : T::AccountId = account("recipient", 0, SEED);
+		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent.clone(), 1u32, 80u32)?;
+	}: _(RawOrigin::Signed(caller.clone()), recipent.clone())
 	verify {
 		assert_last_event::<T>(Event::<T>::PaymentCancelled(caller, recipent).into());
 	}
 
-	// resolve an existing payment succesfully - cancel since that is the most complex route
+	// // resolve an existing payment succesfully - cancel since that is the most complex route
 	resolve {
-		let caller: T::AccountId = setup_caller::<T>();
-		let recipent = account("recipient", 0, SEED);
-		let (asset, amount) = get_asset_and_balance::<T>(1,5);
-		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent, asset, amount);
-	}: _(RawOrigin::Signed(caller), caller, recipent, PaymentState::Cancelled)
+		let caller = whitelisted_caller();
+		let _ = T::Asset::deposit(1u32, &caller, 100u32);
+		let recipent : T::AccountId = account("recipient", 0, SEED);
+		Payment::<T>::create(RawOrigin::Signed(caller.clone()).into(), recipent.clone(), 1u32, 80u32)?;
+	}: _(RawOrigin::Signed(caller.clone()), caller.clone(), recipent.clone(), PaymentState::Cancelled)
 	verify {
 		assert_last_event::<T>(Event::<T>::PaymentCancelled(caller, recipent).into());
 	}
