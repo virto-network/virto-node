9ceaa8aeb653b933dc5fd142c8c72fa5238bc972
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2023-09-04 02:59:46 -0500

    feat(pallets): pallet-communities

---
+++ pallets/communities/src/mock.rs
@@ -0,0 +1,124 @@
+use crate as pallet_communities;
+use frame_support::{
+	parameter_types,
+	traits::{AsEnsureOriginWithArg, ConstU128, ConstU16, ConstU32, ConstU64},
+	PalletId,
+};
+use frame_system::{EnsureRoot, EnsureSigned};
+use sp_core::H256;
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage,
+};
+
+type Block = frame_system::mocking::MockBlock<Test>;
+type Balance = u128;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test
+	{
+		Assets: pallet_assets,
+		Balances: pallet_balances,
+		System: frame_system,
+		Communities: pallet_communities,
+	}
+);
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Nonce = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Block = Block;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ConstU16<42>;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl pallet_assets::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type Balance = Balance;
+	type AssetId = u32;
+	type AssetIdParameter = codec::Compact<u32>;
+	type Currency = Balances;
+	type CreateOrigin = AsEnsureOriginWithArg<EnsureSigned<Self::AccountId>>;
+	type ForceOrigin = EnsureRoot<Self::AccountId>;
+	type AssetDeposit = ConstU128<100>;
+	type AssetAccountDeposit = ConstU128<1>;
+	type MetadataDepositBase = ConstU128<10>;
+	type MetadataDepositPerByte = ConstU128<1>;
+	type ApprovalDeposit = ConstU128<1>;
+	type StringLimit = ConstU32<50>;
+	type Freezer = ();
+	type Extra = ();
+	type CallbackHandle = ();
+	type WeightInfo = ();
+	type RemoveItemsLimit = ConstU32<1000>;
+	#[cfg(feature = "runtime-benchmarks")]
+	type BenchmarkHelper = ();
+}
+
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type RuntimeEvent = RuntimeEvent;
+	type ExistentialDeposit = ConstU128<1>;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ConstU32<10>;
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type RuntimeHoldReason = ();
+	type FreezeIdentifier = ();
+	type MaxHolds = ConstU32<10>;
+	type MaxFreezes = ConstU32<10>;
+}
+
+parameter_types! {
+	pub const CommunitiesPalletId: PalletId = PalletId(*b"kv/comms");
+	pub const CommunitiesFreezeIdentifier: () = ();
+	#[derive(Debug, Clone, PartialEq)]
+	pub const MetadataUrlSize: u32 = 32;
+	#[derive(Debug, Clone, PartialEq)]
+	pub const MaxUrls: u32 = 5;
+	#[derive(Debug, Clone, PartialEq)]
+	pub const MaxLocations: u32 = 2;
+}
+
+impl pallet_communities::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
+	type Assets = Assets;
+	type Balances = Balances;
+	type CommunityId = u128;
+	type MembershipPassport = ();
+	type PalletId = CommunitiesPalletId;
+	type FreezeIdentifier = CommunitiesFreezeIdentifier;
+	type MetadataUrlSize = MetadataUrlSize;
+	type MaxUrls = MaxUrls;
+	type MaxLocations = MaxLocations;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	frame_system::GenesisConfig::<Test>::default()
+		.build_storage()
+		.unwrap()
+		.into()
+}
