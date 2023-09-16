1780f8f36c951ff47dd2b159b0f547456a804303
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2023-09-16 17:24:16 -0500

    change(pallets/communities): use try_mutate_exists on do_insert_member

--- pallets/communities/src/functions/membership.rs
+++ pallets/communities/src/functions/membership.rs
@@ -22,19 +22,19 @@ impl<T: Config> Pallet<T> {
 	}
 
 	pub(crate) fn do_insert_member(community_id: &T::CommunityId, who: &T::AccountId) -> DispatchResult {
-		if <CommunityMembers<T>>::contains_key(community_id, who) {
-			return Err(Error::<T>::AlreadyAMember.into());
-		}
+		<CommunityMembers<T>>::try_mutate_exists(community_id, who, |value| {
+			if value.is_some() {
+				return Err(Error::<T>::AlreadyAMember.into());
+			}
 
-		<CommunityMembers<T>>::insert::<T::CommunityId, T::AccountId, T::MembershipPassport>(
-			community_id.clone(),
-			who.clone(),
-			Default::default(),
-		);
+			// Inserts the member
+			*value = Some(Default::default());
 
-		let members_count = <CommunityMembersCount<T>>::try_get(community_id).unwrap_or_default();
-		<CommunityMembersCount<T>>::set(community_id, members_count.checked_add(1));
+			// Increases member count
+			let members_count = <CommunityMembersCount<T>>::try_get(community_id).unwrap_or_default();
+			<CommunityMembersCount<T>>::set(community_id, members_count.checked_add(1));
 
-		Ok(())
+			Ok(())
+		})
 	}
 }
