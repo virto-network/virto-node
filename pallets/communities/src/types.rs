99cde01de874e33c1ea32d3a1d8d5957e57a6883
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2023-09-19 10:43:52 -0500

    Handling a community-governed treasury account (#299)
    * change(runtime/kreivo): add pallet-communities as dep
    
    * change(pallets/communities): set sp-runtime as dep to support StaticLookup
    
    * feat(pallet/communities): checks for community state
    
    * feat(pallets/communities): check for permissioned calls
    
    * feat(pallets/communities): implement add_member
    
    * change(pallets/communities): use try_mutate_exists on do_insert_member
    
    * chore(community:tests): remove redundant submod
    
    * feat(pallets/communities): implement remove_member
    
    * change(pallets/communities): update documentation
    
    * change(communities:Cargo): update package metadata
    
    * fix(pallets/communities): apply clippy suggestions
    
    * change(pallet/communities): update docs
    
    * fix(communities:remove_member): make sure that origin is being checked on first
    
    * chore(pallets/communities): explicit naming for functions that ensure origin

--- pallets/communities/src/types.rs
+++ pallets/communities/src/types.rs
@@ -2,12 +2,14 @@ use codec::MaxEncodedLen;
 use frame_support::pallet_prelude::{Decode, Encode};
 use frame_support::traits::fungibles::Inspect;
 use frame_support::{sp_runtime::BoundedVec, traits::ConstU32};
-use scale_info::TypeInfo;
+use scale_info::{prelude::vec::Vec, TypeInfo};
+use sp_runtime::traits::StaticLookup;
 
 use crate::Config;
 use frame_system::Config as SystemConfig;
 
 pub type AccountIdOf<T> = <T as SystemConfig>::AccountId;
+pub type AccountIdLookupOf<T> = <<T as SystemConfig>::Lookup as StaticLookup>::Source;
 pub type CommunityIdOf<T> = <T as Config>::CommunityId;
 pub type MembershipPassportOf<T> = <T as Config>::MembershipPassport;
 pub type AssetIdOf<T> = <<T as Config>::Assets as Inspect<AccountIdOf<T>>>::AssetId;
@@ -15,37 +17,65 @@ pub type MemberListOf<T> = Vec<AccountIdOf<T>>;
 
 pub type Cell = u32;
 
-pub type Field<const S: u32> = BoundedVec<u8, ConstU32<S>>;
+pub type SizedField<S> = BoundedVec<u8, S>;
+pub type ConstSizedField<const S: u32> = SizedField<ConstU32<S>>;
 
+/// The Community struct holds the basic definition of a community. It includes
+/// the current state of a community, the [`AccountId`][1] for the community
+/// admin, and (if any) the ID of the community-issued asset the community has
+/// marked to be sufficient.
+///
+/// [1]: `frame_system::Config::AccountId`
 #[derive(TypeInfo, Encode, Decode, MaxEncodedLen)]
 #[scale_info(skip_type_params(T))]
 pub struct Community<T: crate::Config> {
+	/// The [`AccountId`][1] of the current community administrator.
+	///
+	/// [1]: `frame_system::Config::AccountId`
 	pub admin: AccountIdOf<T>,
+	/// The current state of the community.
 	pub state: CommunityState,
+	/// The ID of the asset marked by the community as sufficient.
 	pub sufficient_asset_id: Option<AssetIdOf<T>>,
 }
 
-#[derive(TypeInfo, Encode, Decode, MaxEncodedLen)]
+/// The current state of the community. It represents whether a community
+/// is awaiting to prove their contribution to the network, is active
+/// and can operate, blocked due to a violation of network norms, or
+/// it's being frozen by the community administrators.
+#[derive(Default, TypeInfo, PartialEq, Encode, Decode, MaxEncodedLen)]
 pub enum CommunityState {
+	/// The community is currently awaiting to prove they are contributing
+	/// to the network.
+	#[default]
 	Awaiting,
+	/// The community has proven the required contributions to the network
+	/// and can operate.
 	Active,
+	/// The community is frozen, and is temporality unable to operate. This
+	/// state can be changed by thawing the community via a privileged call.
 	Frozen,
+	/// The community is blocked, typically as a result of a restriction imposed
+	/// by violating the norms of the network. In practice, this is an
+	/// equivalent to being `frozen`, howerver the state cannot be changed by
+	/// the community administrators, but by submitting a proposal to the
+	/// network for it to be changed.
 	Blocked,
 }
 
+/// The CommunityMetadata struct stores some descriptive information about
+/// the community.
 #[derive(TypeInfo, Eq, PartialEq, Debug, Clone, Encode, Decode, MaxEncodedLen)]
 #[scale_info(skip_type_params(T))]
 pub struct CommunityMetadata<T: Config> {
-	pub(super) name: Field<64>,
-	pub(super) description: Field<256>,
-	pub(super) urls: BoundedVec<BoundedVec<u8, T::MetadataUrlSize>, T::MaxUrls>,
-	pub(super) locations: BoundedVec<Cell, T::MaxLocations>,
-}
-
-impl Default for CommunityState {
-	fn default() -> Self {
-		CommunityState::Awaiting
-	}
+	/// The name of the community
+	pub name: ConstSizedField<64>,
+	/// A short description of the community
+	pub description: ConstSizedField<256>,
+	/// A list of URLs that can lead to information about the community
+	pub urls: BoundedVec<SizedField<T::MetadataUrlSize>, T::MaxUrls>,
+	/// A list of physical locations where the community operates.
+	pub locations: BoundedVec<Cell, T::MaxLocations>,
 }
 
 impl<T: Config> Default for CommunityMetadata<T> {
