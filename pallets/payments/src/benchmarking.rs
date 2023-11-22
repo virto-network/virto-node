fb38bbfbecea30ece8f813df3e8e311ac70db14e
Nicolas Fernandez <darkforest0202@gmail.com>
2023-11-22 21:23:16 +0100

    adding release

--- pallets/payments/src/benchmarking.rs
+++ pallets/payments/src/benchmarking.rs
@@ -24,6 +24,54 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
+fn create_accounts<T: Config>() -> (T::AccountId, T::AccountId, AccountIdLookupOf<T>) {
+	let sender: T::AccountId = account("Alice", 0, 1);
+	let beneficiary: T::AccountId = account("Bob", 0, 2);
+	let beneficiary_lookup = T::Lookup::unlookup(beneficiary.clone());
+	(sender, beneficiary, beneficiary_lookup)
+}
+
+fn create_and_mint_asset<T: Config>(
+	sender: &T::AccountId,
+	beneficiary: &T::AccountId,
+	asset: &AssetIdOf<T>,
+	amount: &BalanceOf<T>,
+) -> Result<(), BenchmarkError> {
+	T::BenchmarkHelper::create_asset(asset.clone(), sender.clone(), true, <BalanceOf<T>>::from(1u32));
+	T::Assets::mint_into(asset.clone(), &sender, <BalanceOf<T>>::from(100000u32))?;
+	T::Assets::mint_into(asset.clone(), &beneficiary, <BalanceOf<T>>::from(100000u32))?;
+
+	Ok(())
+}
+
+fn create_payment<T: Config>(
+	amount: &BalanceOf<T>,
+	asset: &AssetIdOf<T>,
+	remark: Option<BoundedDataOf<T>>,
+) -> Result<(T::PaymentId, T::AccountId, T::AccountId, AccountIdLookupOf<T>), BenchmarkError> {
+	let (sender, beneficiary, beneficiary_lookup) = create_accounts::<T>();
+	create_and_mint_asset::<T>(&sender, &beneficiary, &asset, &<BalanceOf<T>>::from(100000u32))?;
+
+	let payment_id: T::PaymentId = Payments::<T>::next_payment_id()?;
+
+	let payment_detail = Payments::<T>::create_payment(
+		&sender,
+		&beneficiary,
+		asset.clone(),
+		amount.clone(),
+		PaymentState::Created,
+		T::IncentivePercentage::get(),
+		remark.as_ref().map(|x| x.as_slice()),
+	)?;
+
+	// reserve funds for payment
+	Payments::<T>::reserve_payment_amount(&sender, &beneficiary, payment_detail)?;
+
+	// TODO: check storage items
+
+	Ok((payment_id, sender, beneficiary, beneficiary_lookup))
+}
+
 #[benchmarks(
 	where
 		<<T as Config>::Assets as Inspect<<T as frame_system::Config>::AccountId>>::AssetId: Zero,
@@ -33,11 +81,9 @@ mod benchmarks {
 
 	#[benchmark]
 	fn pay(q: Linear<1, { T::MaxRemarkLength::get() }>) -> Result<(), BenchmarkError> {
-		let sender: T::AccountId = account("Alice", 0, 1);
-		let beneficiary: T::AccountId = account("Bob", 0, 2);
-		T::BenchmarkHelper::set_balance(beneficiary.clone(), <BalanceOf<T>>::from(100_000_000_u32));
-		let beneficiary_lookup = T::Lookup::unlookup(beneficiary.clone());
-		let asset = <AssetIdOf<T>>::zero();
+		let (sender, beneficiary, beneficiary_lookup) = create_accounts::<T>();
+		let asset: AssetIdOf<T> = <AssetIdOf<T>>::zero();
+		create_and_mint_asset::<T>(&sender, &beneficiary, &asset, &<BalanceOf<T>>::from(100000u32))?;
 		let amount = <BalanceOf<T>>::from(50_u32);
 
 		let remark: Option<BoundedDataOf<T>> = if q == 0 {
@@ -45,9 +91,6 @@ mod benchmarks {
 		} else {
 			Some(BoundedVec::try_from(vec![1 as u8; q as usize]).unwrap())
 		};
-
-		T::BenchmarkHelper::create_asset(asset.clone(), sender.clone(), true, <BalanceOf<T>>::from(1u32));
-		T::Assets::mint_into(asset.clone(), &sender, <BalanceOf<T>>::from(100000u32))?;
 
 		#[extrinsic_call]
 		_(
@@ -68,6 +111,19 @@ mod benchmarks {
 			}
 			.into(),
 		);
+		Ok(())
+	}
+
+	#[benchmark]
+	fn release() -> Result<(), BenchmarkError> {
+		let amount = <BalanceOf<T>>::from(50_u32);
+		let asset = <AssetIdOf<T>>::zero();
+		let (payment_id, sender, beneficiary, beneficiary_lookup) = create_payment::<T>(&amount, &asset, None)?;
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(sender.clone()), beneficiary_lookup, payment_id);
+
+		assert_last_event::<T>(Event::PaymentReleased { sender, beneficiary }.into());
 		Ok(())
 	}
 
