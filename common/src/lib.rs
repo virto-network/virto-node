8aa4947e5e3c6078b087420bc713c105a2bed446
Daniel Olano <daniel@olanod.com>
2023-12-16 09:15:53 +0100

    Fix virto-common compilation when used as dependency of runtime

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
-#![cfg_attr(nightly, feature(ascii_char))]
+#![cfg_attr(feature = "nightly", feature(ascii_char))]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
