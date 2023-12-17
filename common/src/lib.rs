564989b551542a14c900ec13520a90f6237a2334
Daniel Olano <daniel@olanod.com>
2023-12-17 23:31:30 +0100

    Add membership to common types

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -5,5 +5,14 @@
 extern crate alloc;
 extern crate sp_io;
 
+#[cfg(feature = "runtime")]
+use frame_support::pallet_prelude::{Decode, Encode, MaxEncodedLen, TypeInfo};
+
+mod membership;
 mod payment_id;
+pub use membership::{MembershipId, MembershipInfo};
 pub use payment_id::PaymentId;
+
+#[cfg_attr(feature = "runtime", derive(Decode, Encode, MaxEncodedLen, TypeInfo))]
+#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
+pub struct CommunityId(u16);
