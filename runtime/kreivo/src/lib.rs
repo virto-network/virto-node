7676815ceca5d3a8638a5a3b84d7b220891eff89
Daniel Olano <daniel@olanod.com>
2024-01-16 11:09:17 +0100

    Fix integrity tests

--- runtime/kreivo/src/lib.rs
+++ runtime/kreivo/src/lib.rs
@@ -282,7 +282,7 @@ impl pallet_balances::Config for Runtime {
 	type ReserveIdentifier = [u8; 8];
 	type RuntimeHoldReason = RuntimeHoldReason;
 	type FreezeIdentifier = ();
-	type MaxHolds = ConstU32<0>;
+	type MaxHolds = ConstU32<2>;
 	type MaxFreezes = ConstU32<0>;
 	type RuntimeFreezeReason = RuntimeFreezeReason;
 }
