282e14fe92e776cacb85e73feb49be9d937e921d
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-01-26 10:41:13 -0500

    chore(pallet-communities): add initial set of tests for governance section

--- pallets/communities/src/tests/mod.rs
+++ pallets/communities/src/tests/mod.rs
@@ -1,9 +1,13 @@
 use frame_support::assert_ok;
 
+mod helpers;
 mod mock;
+
+use helpers::next_block;
 use mock::*;
 
 type Error = crate::Error<Test>;
 
+mod governance;
 mod membership;
 mod registry;
