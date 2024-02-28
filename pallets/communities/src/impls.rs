f1c3057af354d3ab4f3095b1f6774a9c791a6d48
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-02-28 13:33:23 -0500

    feat(pallet-communities): benchmark for set_metadata / set_decision_method

--- pallets/communities/src/impls.rs
+++ pallets/communities/src/impls.rs
@@ -1,10 +1,15 @@
 use frame_support::traits::VoteTally;
-use sp_runtime::Perbill;
+use sp_runtime::{traits::StaticLookup, Perbill};
 
 use crate::{
 	types::{CommunityIdOf, Tally, VoteWeight},
 	Config,
 };
+
+#[cfg(feature = "runtime-benchmarks")]
+use crate::{CommunityIdFor, Pallet};
+#[cfg(feature = "runtime-benchmarks")]
+use frame_support::traits::membership::Inspect;
 
 impl<T: Config> VoteTally<VoteWeight, CommunityIdOf<T>> for Tally<T> {
 	fn new(_cid: CommunityIdOf<T>) -> Self {
@@ -58,5 +63,23 @@ impl<T: Config> VoteTally<VoteWeight, CommunityIdOf<T>> for Tally<T> {
 	}
 
 	#[cfg(feature = "runtime-benchmarks")]
-	fn setup(class: CommunityIdOf<T>, granularity: Perbill) {}
+	fn setup(community_id: CommunityIdOf<T>, granularity: Perbill) {
+		let community_account_id = Pallet::<T>::community_account(&community_id);
+		let community_origin = CommunityIdFor::<T>::iter_keys()
+			.find(|key| CommunityIdFor::<T>::get(key) == Some(community_id))
+			.expect("find a community origin by its id");
+		let origin = community_origin.into();
+		let voters = granularity.saturating_reciprocal_mul(1u32);
+
+		// Worst case scenarios can be best assessed via Rank decision method
+		for i in 0..voters {
+			let account_id: T::AccountId = frame_benchmarking::account("ranked_collective_benchmarking", i, 0);
+			let who = T::Lookup::unlookup(account_id);
+			let membership_id = T::MemberMgmt::account_memberships(&community_account_id)
+				.next()
+				.expect("has enough memberships");
+			Pallet::<T>::add_member(origin.clone(), who.clone()).expect("can add member to community");
+			Pallet::<T>::promote_member(origin.clone(), who, membership_id).expect("can add member to community");
+		}
+	}
 }
