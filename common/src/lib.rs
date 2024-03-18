333cbfe72467d5beedd1cb7611dd03bb4f7d9593
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-03-17 23:59:51 -0500

    change(virto-common): implement CollectionId bounded traits to CommunityId

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,13 +4,14 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+use core::ops::Add;
+
 #[cfg(feature = "runtime")]
 use frame_support::pallet_prelude::{Decode, Encode, MaxEncodedLen, TypeInfo};
 
 mod membership;
 mod multilocation_asset_id;
 mod payment_id;
-
 pub use membership::{MembershipId, MembershipInfo};
 pub use multilocation_asset_id::{FungibleAssetLocation, NetworkId};
 pub use payment_id::PaymentId;
@@ -25,5 +26,42 @@ pub struct CommunityId(u16);
 impl CommunityId {
 	pub const fn new(id: u16) -> Self {
 		Self(id)
+	}
+}
+
+impl Zero for CommunityId {
+	fn zero() -> Self {
+		Self(Zero::zero())
+	}
+
+	fn is_zero(&self) -> bool {
+		self.0.is_zero()
+	}
+
+	fn set_zero(&mut self) {
+		// Note: It is not possible to set a community to zero
+	}
+}
+
+impl Add for CommunityId {
+	type Output = CommunityId;
+
+	fn add(self, _: Self) -> Self::Output {
+		// Note: It is not possible, nor it should be possible to operate
+		// community IDs, as they're uniques
+		self
+	}
+}
+
+// What [CommunityId]s can do though (at least for now),
+// is to increment themselves, as that's the required
+// method by pallet Nfts
+impl Incrementable for CommunityId {
+	fn increment(&self) -> Option<Self> {
+		self.0.increment().map(|i| Self(i))
+	}
+
+	fn initial_value() -> Option<Self> {
+		Some(Zero::zero())
 	}
 }
