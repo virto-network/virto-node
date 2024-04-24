4c67b4b951e623f36845ac713b4e5549c1bf3a73
Daniel Olano <daniel@olanod.com>
2024-04-24 08:53:42 +0200

    Require a minimum rank of 1 for communities to vote on the Root track

--- runtime/kreivo/src/collective/mod.rs
+++ runtime/kreivo/src/collective/mod.rs
@@ -1,10 +1,19 @@
 use super::*;
 
 use frame_system::EnsureRootWithSuccess;
+use pallet_ranked_collective::Rank;
 use sp_core::ConstU16;
+use sp_runtime::traits::Convert;
 
 pub mod governance;
 pub mod tracks;
+
+pub struct AtLeastRank<const R: Rank>;
+impl<const R: Rank, T> Convert<T, Rank> for AtLeastRank<R> {
+	fn convert(_track: T) -> Rank {
+		R
+	}
+}
 
 pub type KreivoCollectiveInstance = pallet_ranked_collective::Instance1;
 impl pallet_ranked_collective::Config<KreivoCollectiveInstance> for Runtime {
@@ -22,6 +31,6 @@ impl pallet_ranked_collective::Config<KreivoCollectiveInstance> for Runtime {
 	type DemoteOrigin = EnsureRootWithSuccess<Self::AccountId, ConstU16<65535>>;
 
 	type Polls = KreivoReferenda;
-	type MinRankOfClass = ();
+	type MinRankOfClass = AtLeastRank<1>;
 	type VoteWeight = pallet_ranked_collective::Linear;
 }
