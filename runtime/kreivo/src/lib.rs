17ea111bd37ce24f61b17b45fe42c7a2adda4c8c
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-06-14 16:04:22 -0500

    fix(kreivo-runtime): output WASM in builds

--- runtime/kreivo/src/lib.rs
+++ runtime/kreivo/src/lib.rs
@@ -1,11 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit.
-#![recursion_limit = "512"]
+#![recursion_limit = "256"]
 #![allow(clippy::items_after_test_module)]
 
 // // Make the WASM binary available.
-// #[cfg(feature = "std")]
-// include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+#[cfg(feature = "std")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 #[cfg(test)]
 mod tests;
